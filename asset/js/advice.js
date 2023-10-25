fdb3b161cc49ac53f689ad52e01a1f6af91a7bc3
Engrloverboy <samsonanu@yahoo.com>
2023-10-25 15:30:18 +0100

    Done

--- asset/js/advice.js
+++ asset/js/advice.js
@@ -2,17 +2,27 @@ const adviceId = document.querySelector("#adviceId");
 const advicetext = document.querySelector("#advicetext");
 const btn = document.querySelector("#btn");
 
-fetch("https://api.adviceslip.com/advice")
-  .then((response) => {
-    console.log(response.json());
-  })
-  .then((adviceData) => {
-    const adviceNum = adviceData.slip.id;
-    const advice = adviceData.slip.advice;
+function getAdvice() {
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => {
+      return response.json();
+    })
+    .then((adviceData) => {
+      const adviceNum = adviceData.slip.id;
+      const advice = adviceData.slip.advice;
 
-    adviceId.textContent = adviceNum;
-    adviceText.innerHTML = `<p>${advice}</p>`;
-  })
-  .catch((error) => {
-    console.log(Error);
-  });
+      adviceId.textContent = adviceNum;
+      advicetext.innerHTML = `<p>${advice}</p>`;
+    })
+    .catch((error) => {
+      console.log(Error);
+    });
+}
+
+btn.addEventListener("click", function () {
+  getAdvice();
+});
+
+window.onload = () => {
+  getAdvice();
+};
